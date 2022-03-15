41390a5f7505651802f6b1db4eff94646bcfae7a
Kev <100586415+k3vsteel@users.noreply.github.com>
2022-03-15 11:07:32 +0100

    further steps in video
    - Icon Effekt für neue Nachrichten hinzugefügt
    - addResponseMessage hinzugefügt
    - handleNewUserMessage hinzugefügt

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,17 @@
-import {Widget} from "react-chat-widget";
+import {useEffect} from "react";
+import {Widget, addResponseMessage} from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
+
 const Home = () => {
+    useEffect(() => {
+    addResponseMessage("Hey ich bin Liefy der Lieferbot. Kann ich dir weiterhelfen?");
+    }, []);
+
+
+    const handleNewUserMessage = (newMessage) => {
+        console.log(`New message incoming! ${newMessage}`);
+    };
+    
     return (
         <div className="container">
             <div className="row">
@@ -12,7 +23,7 @@ const Home = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam blanditiis cum debitis facilis ipsum magnam praesentium qui quo ratione soluta tempora ullam, unde vero voluptatibus. Alias enim placeat quisquam.</p>
 
             </div>
-            <Widget />
+            <Widget handleNewUserMessage={handleNewUserMessage}/>
         </div>
     );
 };
