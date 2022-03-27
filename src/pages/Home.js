bd0fb8a0cabcd6fcfb11c696a251a41f6db6b1ed
schmcklr <100799533+schmcklr@users.noreply.github.com>
2022-03-27 15:12:01 +0200

    Home.js - switched to rest api instead of socket.io - GET/POST methods - JSON.stringify method - handle User Message method - add response message method

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,22 @@ import {useEffect} from "react";
 import {Widget, addResponseMessage} from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 
+//socket io connection which was used before -> switched to Rest Api
 //Import socket client and Connect to RASA server
+/*
 import {io} from "socket.io-client";
-const socket = io("http://localhost:5005", {
+const socket = io("http://localhost:5005/webhooks/rest/webhook", {
         pathname: '/socket.io',
         transports: ['websocket'],
     });
-//const socket = io("http://localhost:5005");
-//const SERVER = "http://localhost:5005";
-//const socket = io(SERVER, { transports: ["websocket"] });
-
-
-
-
+*/
 const Home = () => {
 
+    //******************************************************************
+    //socket io connection which was used before -> switched to Rest Api
+    //******************************************************************
 
+    /*
     //Connecting with Server - positive response
     socket.on('connect', function () {
         console.log("Connected to Socket.io server");
@@ -28,12 +28,15 @@ const Home = () => {
         console.error(error);
     });
 
+    /*
     // send messages from client to server
     const handleNewUserMessage = (newMessage) => {
         console.log(newMessage);
         socket.emit("user_uttered", newMessage);
+        socket.on("bot_uttered", (message) => {
+        addResponseMessage(message);
+    });
     };
-
 
     //bot responses
     const messages = document.getElementById('messages');
@@ -66,6 +69,78 @@ const Home = () => {
 */
 
 
+    //******************************************************************
+    //Rest Api connection in use
+    //******************************************************************
+
+    //********************GET*********************
+    //GET request using fetch() currently not in use
+    /*
+
+
+    fetch("http://localhost:5005/webhooks/rest/webhook")
+
+        // Converting received data to JSON
+        .then(response => response.json())
+        .then(json => {
+
+        // Create a variable to store HTML
+        let li = `<tr><th>Name</th><th>Email</th></tr>`;
+
+        // Loop through each data and add a table row
+        json.forEach(user => {
+            li += `<tr>
+                <td>${user.name} </td>
+                <td>${user.email}</td>        
+            </tr>`;
+        });
+
+    // Display result
+    //document.getElementById("users").innerHTML = li;
+
+        addResponseMessage(li);
+
+});
+
+*/
+
+
+
+
+
+    // handle user Message typed in via keyboard
+    const handleNewUserMessage = (newMessage) => {
+
+        //********************POST*********************
+        // POST request using fetch() (currently used for sending/receiving messages)
+        fetch("http://localhost:5005/webhooks/rest/webhook", {
+
+        // Adding method type
+        method: "POST",
+
+        // Adding body or contents to send
+        body: JSON.stringify({
+            sender: "test_user",
+            //message which was typed in via keyboard
+            message: newMessage
+        }),
+
+        // Adding headers to the request
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+
+    // Converting to JSON
+    .then(response => response.json()
+
+    )
+
+    // Displaying results in chat widget
+    .then(json => addResponseMessage(json[0].text))
+
+    };
+
 
     return (
         <div className="container">
@@ -76,17 +151,22 @@ const Home = () => {
                 <h2 className="">Wie funktioniert Liefy?</h2>
                 <hr/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam blanditiis cum debitis
-                    facilis ipsum magnam praesentium qui quo ratione soluta tempora ullam, unde vero voluptatibus. Alias
-                    enim placeat quisquam.</p>
+                        facilis ipsum magnam praesentium qui quo ratione soluta tempora ullam, unde vero voluptatibus. Alias
+                        enim placeat quisquam.</p>
 
             </div>
             <Widget handleNewUserMessage={handleNewUserMessage}
                     initPayload={"/get_started"}
-                    socketUrl={"http://localhost:5005"}
+                // socketUrl={"http://localhost:5005"}
                     socketPath={"/socket.io/"}
                     customData={{"language": "en"}}
-                    />
+                    params={{'storage':'session'}}
+                    title={"Liefy der Chatbot"}
+                    subtitle={"How can I help you?"}
+                    showTimeStamp={"yes"}
+
+            />
         </div>
-    );
-};
-export default Home;
+        );
+    };
+    export default Home;
