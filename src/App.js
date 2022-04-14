ded9e710a2102ed93a3908de88468befa6f166ee
schmcklr <100799533+schmcklr@users.noreply.github.com>
2022-04-14 20:22:25 +0200

    Navbar, new Website design

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Routes
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Routes
 } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Contact from "./pages/Contact";
-import {Container, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 
 
-
+/*
 const Nav = () => <ul className="nav justify-content-center">
     <li className="nav-item">
         <a className="nav-link active" aria-current="page" href="/">Home</a>
@@ -31,20 +31,40 @@ const Nav = () => <ul className="nav justify-content-center">
     </li>
 </ul>
 
+*/
 
 
+const Navigation = () => {
+    return (
+        <>
+            <Navbar bg="dark" variant="dark" color='rgba(174,0,0,1)'>
+                <Container>
+                    <Navbar.Brand href="/">Liefy</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+        </>
+    )
+
+
+}
 
 function App() {
-  return (
-      <Router>
-            <Nav />
+    return (
+        <div className="App2">
+            <Navigation/>
             <Routes>
-                <Route exact path="/" element={<Home />}/>
-                <Route exact path="/about" element={<About />}/>
-                <Route exact path="/contact" element={<Contact />}/>
+                <Route exact path="/" element={<Home/>}/>
+                <Route exact path="/about" element={<About/>}/>
+                <Route exact path="/contact" element={<Contact/>}/>
             </Routes>
-        </Router>
-
-  );
+        </div>
+    );
 }
+
 export default App;
