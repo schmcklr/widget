5b04b1841eae89a432ff68496af647b7484a33a0
schmcklr <100799533+schmcklr@users.noreply.github.com>
2022-04-14 16:31:02 +0200

    Restructure of the Project - implemented customComponents like Cards, Carousels and Card Groups

--- src/customComponents/CustomCarousel.js
+++ src/customComponents/CustomCarousel.js
@@ -1,44 +1,62 @@
 import {Component} from "react";
-import {Carousel} from "react-bootstrap";
+import {Card, Carousel} from "react-bootstrap";
+import "./tets.css";
 
 export default class CustomCarousel extends Component {
     render() {
-        return (<Carousel>
+        return (<Carousel  id="myCarousel" variant="dark" indicators={false} slide={false}>
             <Carousel.Item interval={1000}>
-                <img
-                    className="d-block w-100"
-                    src="https://medien.bremen.de/media/464/288/italienisch-essen-bruschetta-quelle--beats-.jpg"
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                <Card style={{width: '15rem'}}>
+                    <Card.Img variant="top"
+                              src="https://image.geo.de/30131936/t/zp/v3/w1440/r0/-/vollkornbuerger-f-80996477-jpg--74900-.jpg"/>
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
             </Carousel.Item>
             <Carousel.Item interval={500}>
-                <img
-                    className="d-block w-100"
-                    src="https://medien.bremen.de/media/464/288/italienisch-essen-bruschetta-quelle--beats-.jpg"
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+                <Card style={{width: '15rem'}}>
+                    <Card.Img variant="top"
+                              src="https://medien.bremen.de/media/464/288/italienisch-essen-bruschetta-quelle--beats-.jpg"/>
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+
             </Carousel.Item>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://medien.bremen.de/media/464/288/italienisch-essen-bruschetta-quelle--beats-.jpg"
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
+                <Card style={{width: '15rem'}}>
+                    <Card.Img variant="top"
+                              src="https://media.istockphoto.com/photos/arabic-and-middle-eastern-dinner-table-hummus-tabbouleh-salad-salad-picture-id1175505781?k=20&m=1175505781&s=612x612&w=0&h=STomby2lCtcvpl_hxK6RhknQQWrkvpkHcoDLD4zttFk="/>
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
             </Carousel.Item>
         </Carousel>)
 
     }
+
 }
 
