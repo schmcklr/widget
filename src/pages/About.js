df397f384d7d3d8ab275586312b875e061a8dbb6
schmcklr <100799533+schmcklr@users.noreply.github.com>
2022-05-07 18:14:32 +0200

    - major design updates for cards, messages, input messages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className="container">
             <div className="row">
-                <h1 className="text-center p-5 display-1">About Page</h1>
+                 <h1 className="text-left p-5"><br/>About Page</h1>
                 <hr />
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam blanditiis cum debitis facilis ipsum magnam praesentium qui quo ratione soluta tempora ullam, unde vero voluptatibus. Alias enim placeat quisquam.</p>
             </div>
